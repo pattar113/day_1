465118443f53221ab4da9b2c1d1a302b757d7fa1
pattar113 <pattar.dilm@gmail.com>
2020-04-28 12:23:05 -0700

    AddCommend

---
+++ src/day_1/AddCommand.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class AddCommand {
+    public static void main(String[] args) {
+
+
+        // i will see you GitHub
+        // i made some change here when i create .
+
+    }
+
+}
